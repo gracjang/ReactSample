663208f5d82dfefc8e5aa5bd41ae3dbb031f6a2d
Gracjan <graku12@gmail.com>
2020-09-20 12:02:27 +0200

    prettier & esLint

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -16,7 +18,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React kkEirmod nonumy duo kasd et at est diam rebum diam et, eirmod duo no voluptua
+          lorem no clita sea no. Eirmod amet no et no justo ut clita et. Sed sit sit justo labore
+          duo, vero ut tempor ipsum rebum sit, ipsum invidunt duo sea erat at, diam et labore et
+          amet lorem eos eos takimata elitr, amet sadipscing aliquyam eirmod labore sit no no.
+          Voluptua consetetur sanctus dolor lorem aliquyam, amet ipsum est sed eirmod, nonumy dolor
+          stet kasd et dolores clita diam. Labore magna invidunt sanctus duo voluptua sit sanctus
+          sit aliquyam, justo amet dolore sanctus no lorem.
         </a>
       </header>
     </div>
