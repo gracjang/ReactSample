0e2476cb03901af43503a5c2bce02114cb72cc30
Gracjan <graku12@gmail.com>
2020-09-26 15:13:30 +0200

    Added logo and linkIcon to card

--- src/components/molecules/Card/Card.js
+++ src/components/molecules/Card/Card.js
@@ -4,11 +4,12 @@ import PropTypes from 'prop-types';
 import Heading from 'components/atoms/Heading/Heading';
 import Button from 'components/atoms/Button/Button';
 import Paragraph from 'components/atoms/Paragraph/Paragraph';
+import LinkIcon from 'assets/link.svg';
 
 const StyledWrapper = styled.div`
   box-shadow: 0 10px 30px -10px hsla(0, 0%, 0%, 0.1);
   width: 35%;
-  min-height: 450px;
+  min-height: 400px;
   border-radius: 10px;
   overflow: hidden;
   display: grid;
@@ -16,8 +17,13 @@ const StyledWrapper = styled.div`
 `;
 
 const InnerWrapper = styled.div`
-  padding: 17px 30px 10px;
+  padding: 15px 30px 10px;
   background-color: ${({ activeColor, theme }) => (activeColor ? theme[activeColor] : 'white')};
+  position: relative;
+
+  :first-of-type {
+    z-index: 9999;
+  }
 
   ${({ flex }) =>
     flex &&
@@ -38,12 +44,40 @@ const StyledHeading = styled(Heading)`
   margin: 5px 0 0;
 `;
 
+const StyledAvatar = styled.img`
+  width: 86px;
+  height: 86px;
+  border-radius: 50px;
+  border: 5px solid ${({ theme }) => theme.twitter};
+  position: absolute;
+  right: 25px;
+  top: 50px;
+`;
+
+const StyledLinkButton = styled.a`
+  display: block;
+  width: 47px;
+  height: 47px;
+  border-radius: 50px;
+  background: white url(${LinkIcon}) no-repeat;
+  background-size: 60%;
+  background-position: 50%;
+  position: absolute;
+  right: 25px;
+  top: 50%;
+  transform: translateY(-50%);
+`;
+
 const Card = ({ cardType }) => {
   return (
     <StyledWrapper>
       <InnerWrapper activeColor={cardType}>
         <StyledHeading>Siemandero Gracjan</StyledHeading>
         <DateInfo>3 days</DateInfo>
+        {cardType === 'twitter' && (
+          <StyledAvatar src="https://unavatar.now.sh/twitter/MorawieckiM" />
+        )}
+        {cardType === 'article' && <StyledLinkButton href="https://twitter.com/MorawieckiM" />}
       </InnerWrapper>
       <InnerWrapper flex>
         <Paragraph>
